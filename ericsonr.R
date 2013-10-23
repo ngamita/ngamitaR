bed55e0f860dae413fbe833acaaf83e6bf9b01f6
Richard Ngamita <ngamita@gmail.com>
2013-10-23 12:27:50 +0300

    Commit ericson stats

---
+++ ericsonr.R
@@ -0,0 +1,12 @@
+## Automate Monthly Ericson script. 
+## __Author__ 'ngamita@refunite.org'
+## 
+
+## 1. Dump all Mysql stats (Format: )
+## 2. Dump all Mongo stats (Format: )
+
+
+## Move to /dir load rusql.csv
+## Move to /dir load rumongo.csv
+
+
