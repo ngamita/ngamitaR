712004000f91daccdc75a01ad4ca06b5a431d212
Richard Ngamita <ngamita@gmail.com>
2013-10-24 12:46:55 +0300

    loading all stats, date issues still

--- ericsonr.R
+++ ericsonr.R
@@ -1,6 +1,11 @@
 ## Automate Monthly Ericson script. 
 ## __Author__ 'ngamita@refunite.org'
-## 
+
+## ************** Statistics to summarise for Monthly *************
+
+## 1. Accumulated total number of registrations:
+## 2. Number of new registrations:
+## 3. Number of active users** 
 
 ## git stepps: 
 ## git add *
@@ -10,7 +15,7 @@
 
 
 ## *** Defaults *** 
-directory_local <- "/home/ngamita/refunite/R/data"
+directory <- "/home/ngamita/refunite/R/data"
 
 ## 1. Dump all Mysql stats (Format: )
 ## 2. Dump all Mongo stats (Format: )
@@ -20,15 +25,19 @@ directory_local <- "/home/ngamita/refunite/R/data"
 
 file <- paste('rusql', "csv", sep=".")
 path_sql <- paste(directory, file, sep="/")
-print(path_sql)
-#add_sql <- read.csv(path_sql, header=T, sep=',') # load sql dump, Df called add_sql
+#print(path_sql)
+# Fix warning --> set quote = ""
+add_sql <- read.csv(path_sql, header=F, sep=',', quote = "", 
+										row.names = NULL, 
+										stringsAsFactors = FALSE) # load sql dump, Df called add_sql
+head(add_sql)
 
 ## Move to /dir load rumongo.csv
 
 file <- paste('rumongo', "csv", sep=".")
 path_mongo <- paste(directory, file, sep="/")
-print(path_mongo)
+#print(path_mongo)
 add_mongo <- read.csv(path_mongo, header=T, sep=',')  # load Mongo dump, Df called add_mongo
-
+head(add_mongo)
 
 
